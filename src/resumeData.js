6f456b6066817bfa978711cd16955eb5ef8351c7
Anand Bharti <anand.bharti980@gmail.com>
2022-09-22 10:20:22 +0530

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,124 @@
+let resumeData = {
+  imagebaseurl: "https://github.com/anand980/",
+  name: "Anand Bharti",
+  role: "Frontend Developer",
+  linkedinId: " anandbharti980",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/anandbharti980/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/anand980",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "https://www.instagram.com/anandbharti980/",
+      className: "fa fa-instagram"
+    }
+  ],
+  aboutme:
+    "I am a Software Engineer. Currently working with Xoriant. I have worked with Fiserv in past. I graduated (BE Computer Science) from Pune University in 2017",
+  address: "India",
+  website: "https://github.com/anand980",
+  education: [
+    {
+      UniversityName: "Pune University",
+      specialization: "Computer Engineering",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2017"
+      // Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "MGM Bokaro",
+      specialization: "XII",
+      MonthOfPassing: "Apr",
+      YearOfPassing: "2012"
+      // Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "DAV Aurangabad",
+      specialization: "X",
+      MonthOfPassing: "Apr",
+      YearOfPassing: "2010"
+      // Achievements: "Some Achievements"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Xoriant",
+      specialization: "Software Engineer",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2022"
+      // Achievements: "Some Achievements"
+    },
+    {
+      CompanyName: "Fiserv",
+      specialization: "Xoriant",
+      MonthOfLeaving: "Dec",
+      YearOfLeaving: "2017"
+      // Achievements: "Some Achievements"
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+      imgsrc: "html5.svg"
+    },
+    {
+      skillname: "CSS",
+      imgsrc: "css-3.svg"
+    },
+    {
+      skillname: "Reactjs",
+      imgsrc: "react.svg"
+    },
+    {
+      skillname: "Javascript",
+      imgsrc: "icons8-javascript.svg"
+    },
+    {
+      skillname: "Typescript",
+      imgsrc: "icons8-typescript.svg"
+    },
+    {
+      skillname: "Bootstrap",
+      imgsrc: "bootstrap.svg"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Food Cart",
+      description: "A web application for food ordering",
+      link: "https://react-yndcui.stackblitz.io",
+      imgurl:
+        "https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+      name: "Online Food Order",
+      description: "A website similar to Zomato",
+      link: "https://react-ikfkwo.stackblitz.io",
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbNw_vP05soOrrAwLWx7w7JV9769svcqDLOw&usqp=CAU"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
